d14b15e7b67df6c6934aaabdb51083792b01a715
Sanket-Sakhardande <sanket_sakhardande@persistent.com>
2019-08-21 14:24:27 +0530

    another Syso added

--- App.java
+++ App.java
@@ -3,5 +3,8 @@ public class App{
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
         System.out.println("New commit");
+
+        System.out.println("heyy how you doin??");
+
     }
 }
